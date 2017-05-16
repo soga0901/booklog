aed59edded12fbb96d6de721fcb12f1d8dad26e9
soga0901 <sg.0901.sg@gmail.com>
2017-05-16 23:42:43 +0900

    Edit

--- app/assets/javascripts/book.js
+++ app/assets/javascripts/book.js
@@ -1,5 +1,11 @@
 $(function(e) {
-  $(".fa-search").on("click", function() {
-    $(".book-search-btn").click();
+  $("#book-search--header").on("click", function() {
+    console.log(12);
+    $("#book-search-btn-header").click();
+  });
+
+  $("#book-search--body").on("click", function() {
+    console.log("");
+    $("#book-search-btn-body").click();
   });
 });
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,5 @@
 class BooksController < ApplicationController
   def search_result
-    binding.pry
     @search_word = params[:info]
     @books = Book.where('title LIKE(?) OR author LIKE(?)', "%#{params[:info]}%", "#{params[:info]}%").page(params[:page]).per(10)
   end
