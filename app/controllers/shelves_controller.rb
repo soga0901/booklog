660e4f266041d0fecf63b2423f8a8d2875a8db97
soga0901 <sg.0901.sg@gmail.com>
2017-05-28 03:25:18 +0900

    Edit

--- app/controllers/shelves_controller.rb
+++ app/controllers/shelves_controller.rb
@@ -3,6 +3,6 @@ class ShelvesController < ApplicationController
     @shelf = Shelf.find(params[:id])
     @user = User.find(params[:id])
     @comment = Comment.new
-    @comments = @shelf.comments
+    @comments = @shelf.comments.page(params[:page]).per(5).order("created_at DESC")
   end
 end
